a2c5bf3ac9dd371f4980a96e0c952cce1acadf7b
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-25 19:01:13 +0300

    build project

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -7,7 +7,7 @@ import (
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.GET("/users", controllers.GetUser())
+	incomingRoutes.GET("/users", controllers.GetUsers())
 	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
 	incomingRoutes.POST("/users/signup", controllers.SignUp())
 	incomingRoutes.POST("/users/login", controllers.Login())
